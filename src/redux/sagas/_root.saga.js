75cfaf4bb376d797545879e8e5ea2adf4f7ee6fa
ZakariyeAden <zakariye5417@gmail.com>
2023-08-01 12:19:10 -0500

    setting rup reducer and sagas for Verses.Need to target the Id for Chapters

--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -4,6 +4,7 @@ import registrationSaga from './registration.saga';
 import userSaga from './user.saga';
 import chaptersSaga from './chapters.sagas';
 import chapterItemSaga from './chapterItem.sagas';
+import versesSaga from './verses.sagas'
 // rootSaga is the primary saga.
 // It bundles up all of the other sagas so our project can use them.
 // This is imported in index.js as rootSaga
@@ -18,5 +19,6 @@ export default function* rootSaga() {
     userSaga(),
     chaptersSaga(),
     chapterItemSaga(),
+    versesSaga(),
   ]);
 }
---
+++ src/redux/sagas/verses.sagas.js
@@ -0,0 +1,22 @@
+import { put, takeLatest } from "redux-saga/effects";
+import axios from "axios";
+
+// GET the Verses from API
+// Target the id for each chapter in dispach! 
+// And display in DOM for client side
+function* fetchVerses(action) {
+  try {
+    const verses = yield axios.get(`/api/verses/${action.payload}`);
+
+    yield put({ type: "SET_VERSES", payload: verses.data });
+    // Catch any errors
+  } catch (err) {
+    console.log("ERROR in GET sagas verses:", err);
+  }
+}
+
+function* versesSaga() {
+  yield takeLatest("FETCH_VERSES", fetchVerses);
+}
+
+export default versesSaga;
