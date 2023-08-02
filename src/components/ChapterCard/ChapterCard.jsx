9b54c3c21af2f73db406e23fddb8f5366daa7745
ZakariyeAden <zakariye5417@gmail.com>
2023-08-02 15:20:23 -0500

    fixing card

--- src/components/ChapterCard/ChapterCard.jsx
+++ src/components/ChapterCard/ChapterCard.jsx
@@ -9,40 +9,33 @@ import {
   CardContent,
   Button,
   CardActions,
+  Container,
 } from "@mui/material";
 const ChapterCard = ({ chapter }) => {
   const dispatch = useDispatch();
   const history = useHistory();
-  
+
   // Get the Chapter Id and dispatch:
   const getChapterId = id => {
     // Chapter Id selected by user:
     console.log("Chapter Id:", id);
     // Dispatchs for Indivual Chapter and Verses
     dispatch({ type: "SET_CHAPTER_ITEM", payload: id });
-    dispatch({type: 'SET_VERSES', payload: id.id});
+    dispatch({ type: "SET_VERSES", payload: id.id });
     // Route to Verses page
     history.push(`/user/${id.id}`);
   };
   return (
     <div>
-      <Grid item lg={8} md={4} xs={6}>
-        <Card sx={{ minWidth: 375 }} onClick={() => getChapterId(chapter)}>
-          <CardContent className="card">
-            <Typography color="text.secondary">
-              {chapter.chapter_number}
-            </Typography>
-            <Typography variant="h5" component="div">
-              {chapter.name}
-            </Typography>
-            <Typography color="text.secondary">
-              {chapter.arabic_name}
-            </Typography>
-            <Typography color="text.secondary">
-              {chapter.translated_name}
-            </Typography>
-            <Button size="small">Bookmark</Button>
+      <Grid item lg={3} md={4} xs={6}>
+        <Card sx={{ minWidth: 375 }} >
+          <CardContent  onClick={() => getChapterId(chapter)} className="card">
+            <Typography>{chapter.chapter_number}</Typography>
+            <Typography>{chapter.name}</Typography>
+            <Typography>{chapter.arabic_name}</Typography>
+            <Typography>{chapter.translated_name}</Typography>
           </CardContent>
+          <Button size="small" >Bookmark</Button>
         </Card>
       </Grid>
     </div>
