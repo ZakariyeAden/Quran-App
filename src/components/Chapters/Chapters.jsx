3e3446890ab8a3a0448352a27839f0b57f7adcb2
ZakariyeAden <zakariye5417@gmail.com>
2023-08-05 16:38:25 -0500

    fixing Cards in Home screen

--- src/components/Chapters/Chapters.jsx
+++ src/components/Chapters/Chapters.jsx
@@ -3,7 +3,8 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Grid, Container } from "@mui/material";
 import ChapterCard from "../ChapterCard/ChapterCard";
-
+// CSS
+import './Chapters.css'
 const Chapters = () => {
   const dispatch = useDispatch();
   const chapters = useSelector(state => state.chapters);
@@ -11,9 +12,10 @@ const Chapters = () => {
   useEffect(() => {
     dispatch({ type: "FETCH_CHAPTERS" });
   }, []);
+
   return (
     <div>
-      <Grid container spacing={2}>
+      <Grid container spacing={2} className="chapter-card">
         {chapters.map(chapter => {
           return <ChapterCard chapter={chapter} key={chapter.id} />;
         })}
