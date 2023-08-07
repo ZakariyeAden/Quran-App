9ea92617b36507093801c338cf795c5ebc463904
ZakariyeAden <zakariye5417@gmail.com>
2023-08-07 12:30:28 -0500

    setting up reducer and sagas for audio

---
+++ src/redux/reducers/playAudio.reducer.js
@@ -0,0 +1,10 @@
+const versesReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'SET_AUDIO':
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export default versesReducer;
