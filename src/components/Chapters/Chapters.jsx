fe64c23762004bbf14536d9a5e936fc7a7ecef8b
ZakariyeAden <zakariye5417@gmail.com>
2023-08-08 19:06:33 -0500

    working on loader

--- src/components/Chapters/Chapters.jsx
+++ src/components/Chapters/Chapters.jsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 // Components
 import ChapterCard from "../ChapterCard/ChapterCard";
+import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
 // CSS and MUI
 import './Chapters.css'
 import { Grid, Container } from "@mui/material";
@@ -11,13 +12,14 @@ const Chapters = () => {
   const dispatch = useDispatch();
   const chapters = useSelector(state => state.chapters);
   const user = useSelector(state => state.user);
+  const LoadingSpinner = useSelector(store => store.loadingSpinner);
 
   // Load all of the Chapters
   useEffect(() => {
     dispatch({ type: "FETCH_CHAPTERS" });
   }, []);
 
-  return (
+  return  (
     <div className="hero-section">
       <h1 className="user-heading">Welcome, <span className="user">{user.username}</span> to the Noble Quran</h1>
       <Grid container spacing={2} className="chapter-card">
--- src/components/LoadingSpinner/LoadingSpinner.jsx
+++ src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -1,10 +1,14 @@
-import React from "react";
-// Loading Spinner 
+import {useState} from "react";
+// Loading Spinner
 const LoadingSpinner = () => {
   return (
-    <Backdrop sx={{ color: "#fff", zIndex: theme => theme.zIndex.drawer + 1 }}>
-      <CircularProgress color="inherit" />
-    </Backdrop>
+    <div>
+      <Backdrop
+        sx={{ color: "#fff", zIndex: theme => theme.zIndex.drawer + 1 }}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
+    </div>
   );
 };
 
